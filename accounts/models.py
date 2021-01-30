9d8d32326d2a62271c7f9799439d8a0f8e38affb
juliobriones <julesc00@hotmail.com>
2021-01-29 20:28:08 -0600

    Added forms for user creating and login.

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Usuarios(models.Model):
+class Usuario(models.Model):
     """Create custom user model."""
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, null=True, blank=True)
